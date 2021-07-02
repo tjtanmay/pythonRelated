0f234987e71d38579ecd500d58e93b2c44a8cbff
tjtanmay <tjtanmay252gmail.com>
2021-07-01 20:35:01 -0500

    Added elif to DaysTohour.py

--- DaysTohour.py
+++ DaysTohour.py
@@ -5,6 +5,8 @@ name_of_unit = "hours"
 def days_to_units(num_of_days):
 	if num_of_days > 0:
 		return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+	elif num_of_days == 0:
+		return f"enter number greater than 0"
 	else:
 		return "You have enterd a negative value!"
 
