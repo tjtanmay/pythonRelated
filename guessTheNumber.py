bae8e0d78f20327bb62313e508493bea5d9bac80
tjtanmay <tjtanmay252gmail.com>
2021-07-07 01:27:48 -0500

    Reverse string program

---
+++ guessTheNumber.py
@@ -0,0 +1,24 @@
+import random
+import math
+upper=int(input("Enter range, Upper bound:\n"))
+lower=int(input("Lower bound:\n"))
+x = int(random.randint(lower, upper))
+chance_left= round(math.log(upper - lower + 1, 2))
+print("\n\tYou've only ",
+       round(math.log(upper - lower + 1, 2)),
+      " chances to guess the integer!\n")
+count=0
+while (count < chance_left):
+	count +=1
+	print (f"guess number {count}:")
+	guess=int(input("enter your guess: \n"))
+	
+	if guess == x:
+		print("Congratulations!")
+	elif guess < x:
+		print("Try Again! You guessed too small")
+	elif guess > x:
+		print("Try Again! You guessed too high")
+	elif count==chance_left:
+		print("Better Luck Next Time!")
+		break;
